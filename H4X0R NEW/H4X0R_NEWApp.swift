b1297ab786f25ae219fd5b78d2e06bbb0a899cfc
maitrongnhan001 <maitrongnhan.001@gmail.com>
2024-06-19 22:41:50 +0700

    Initial Commit

---
+++ H4X0R NEW/H4X0R_NEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NEWApp.swift
+//  H4X0R NEW
+//
+//  Created by Nhan Mai on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
